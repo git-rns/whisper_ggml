bc95de3d576960392529c035a57669cf39332405
Richard Novas <richard.novas@nativewit.in>
2025-06-25 17:25:14 +0530

    fix[app]: removed whisper_audio_convert.dart and   ffmpeg_kit_flutter_new: ^1.6.1

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import ffmpeg_kit_flutter_new
 import path_provider_foundation
 import record_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
 }
